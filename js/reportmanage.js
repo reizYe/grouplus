06157ed01fa07c6aabd71d1c8f22f26d6615ae5f
reiz <1359677939@qq.com>
2017-08-26 12:21:35 +0800

    使用ajax()

--- js/reportmanage.js
+++ js/reportmanage.js
@@ -1,55 +1,54 @@
 $(function () {
+    //加载动画
     $(document).ajaxStart(function () {
         $('.loader').css('display', 'block');
     });
     $(document).ajaxComplete(function () {
         $('.loader').css('display', 'none');
     });
+    //加载数据请求
     update = function (num) {
-        $.get("php/reportmanage.php?t="+Math.random(),
-            function (data) {
-                var html = "";
-                var int = 0;
-                var number = (num == 0) ? $('.left-foot>select').val() - 1 : num;
-                //  limit 10
-                $.each(data, function (index, item) {
-                    int++;
-                    html += '<tr>' +
-                        '<td class="table-tr"><span><strong><a>' + item['title'] + '</a></strong></span></td>' +
-                        '<td><span>' + item['time'] + '</span></td>' +
-                        '<td><span>' + item['status'] + '</span></td>' +
-                        '<td><span>' + item['read'] + '</span></td>' +
-                        '<td><span>' + item['newtickling'] + '</span></td>' +
-                        '<td><span>' + item['alltickling'] + '</span></td>' +
-                        '<td><span><a>' + '管理' + '</a> &nbsp;' + '<a class="delete">' + '删除' + '</a></span></td>' +
-                        '</tr>';
-                    if (index == number) {
-                        return false;
-                    };
-                    
-                });
-                $('tbody').append(html);
-                $('.left-foot>span').text(int);
-                
-            }
-        ).done();
-    }
+         var html = "";
+         var int = 0;
+         var number = (num == 0) ? $('.left-foot>select').val() - 1 : num;
+        $.get("php/reportmanage.php").promise().done(function (data) {
+            //Ajax使用Promise方法(默认返回一个Promise对象，所以可以不必显式调用Promise方法)
+           
+            //  limit 10
+            $.each(data, function (index, item) {
+                int++;
+                html += '<tr>' +
+                    '<td class="table-tr"><span><strong><a>' + item['title'] + '</a></strong></span></td>' +
+                    '<td><span>' + item['time'] + '</span></td>' +
+                    '<td><span>' + item['status'] + '</span></td>' +
+                    '<td><span>' + item['read'] + '</span></td>' +
+                    '<td><span>' + item['newtickling'] + '</span></td>' +
+                    '<td><span>' + item['alltickling'] + '</span></td>' +
+                    '<td><span><a>' + '管理' + '</a> &nbsp;' + '<a class="delete">' + '删除' + '</a></span></td>' +
+                    '</tr>';
+                if (index == number) {
+                    return false;
+                };
+            });
+            //可以改为promise
+            $('tbody').append(html);
+            $('.left-foot>span').text(int);
 
-    update(9);
+        });
+    }
+    //  $('.left-foot>span').text(int);
     $('.left-foot>select').change(function () {
         var num = $('.left-foot>select').val() - 1;
         $('tbody').empty();
         update(num);
 
     });
- 
+    $(".delete").promise().done($("tbody>tr").on('click', function () {
+            console.log(111);
+        
+    }));
+    update(9);
 });
-$(document).ready(function(){
-      $(".delete").on('click', function () {
-          try {
-              console.log(111);
-          } catch (error) {
-              alert(error);
-          }
-      });
+$(function(){
+    
 });
